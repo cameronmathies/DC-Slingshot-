856b9d8aec5ef62c8e60cee36fe8e6c91d15af8b
cameronmathies <cmathies@utc4me.org>
2016-09-19 10:48:18 -0400

    Main Menu Done
    Just need to fix quitting code.

---
+++ DC Slingshot Project/Assets/Menu/Menu Code/MainMenuButtons.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public void Play(){
+		Application.LoadLevel ("Levels");
+}
+
+	public void Quit(){
+//		Application.Quit;
+
+	}
+}
